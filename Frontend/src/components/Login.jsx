f0a684ef212c78c0d25fb0e6d1f1c179f37f17dc
Sakshi Sharma <sharmasakshi8546@gmail.com>
2024-07-03 22:46:59 +0530

    Backend Done

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { useForm } from "react-hook-form"
+import axios from "axios"
+import toast from 'react-hot-toast';
 
 function Login() {
 
@@ -10,7 +12,35 @@ function Login() {
     formState: { errors },
   } = useForm();
 
-  const onSubmit = (data) => console.log(data);
+  const onSubmit = async (data) => {
+    const userInfo = {
+      email: data.email,
+      password: data.password,
+    }
+    await axios.post("http://localhost:4001/user/login", userInfo)
+      .then((res) => {
+        console.log(res.data)
+        if (res.data) {
+
+          toast.success("Loggedin successfully");
+          document.getElementById("my_modal_3").close();
+          setTimeout(() => {
+            
+            window.location.reload();
+            localStorage.setItem("Users", JSON.stringify(res.data.user));
+            }, 1000);
+           }
+
+      })
+      .catch((err) => {
+        if (err.response) {
+          console.log(err)
+
+          toast.error("Error:" + err.response.data.message);
+          setTimeout(()=>{},2000);
+        }
+      });
+  }
 
   return (
     <div>
@@ -20,7 +50,9 @@ function Login() {
             {/* if there is a button in form, it will close the modal */}
             <Link
               to="/"
-              className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+              className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+              onClick={() => document.getElementById("my_modal_3").close()}
+            >
               ✕
             </Link>
 
@@ -47,7 +79,7 @@ function Login() {
                 {...register("password", { required: true })}
               />
               <br />
-              {errors.password &&(  <span className="text-sm text-red-500">This field is required</span>)}
+              {errors.password && (<span className="text-sm text-red-500">This field is required</span>)}
             </div>
             {/*Button*/}
             <div className="flex justify-around mt-4">
